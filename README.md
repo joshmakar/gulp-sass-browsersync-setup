14d64908afee3a3b8e99a3fb5cdaa9d7acdee122
joshmakar <joshmakar@gmail.com>
2019-08-15 12:03:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Gulp, SASS, Browser Sync Basic Configuration
+
+Install the Gulp Command Line Interface
+
+`$ npm install gulp-cli -g`
+
+Create a package.json file
+
+`$ npm init`
+
+Install Gulp, Gulp SASS, and Browser Sync
+
+`$ npm i -D gulp gulp-sass browser-sync`
+
+Create the Gulp File
+
+`/gulpfile.js`  
+or  
+`/gulpfile.js/index.js`
---
+++ gulpfile.js/index.js
@@ -0,0 +1,26 @@
+const gulp        = require('gulp'),
+      sass        = require('gulp-sass'),
+      browserSync = require('browser-sync').create();
+
+
+// Compile SCSS into CSS
+function style() {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch('./scss/**/*.scss', style);
+  gulp.watch('./**/*.html').on('change', browserSync.reload);
+  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
